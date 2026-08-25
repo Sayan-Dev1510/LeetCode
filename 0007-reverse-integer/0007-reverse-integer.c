03fbad3971c79a7ae8217cafb10958769b90cfdf
Sayan <sayankabasi0@gmail.com>
2026-08-25 12:23:27 -0700

    Time: 7 ms (9.06%), Space: 9.3 MB (9.15%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,17 @@
+int reverse(int x){
+    int rev = 0 ;
+    int d ; 
+    while( x != 0 ){
+        d = x % 10 ;
+        x = x / 10 ;
+        if ((rev > INT_MAX / 10) || ( rev == INT_MAX /10 && d > 7)){
+            return 0 ; 
+        }
+
+        if ((rev < INT_MIN / 10 ) || ( rev == INT_MIN / 10 && d < -8 )){
+            return 0 ;
+        }
+        rev = rev * 10 + d ; 
+    }
+    return rev ;
+}
